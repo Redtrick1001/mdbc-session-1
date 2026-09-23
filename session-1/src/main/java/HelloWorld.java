64a3f1f50b35a5923147b39371f6b0c7be9bcc17
thomas gasseling <thomas.gasseling@autotrader.co.uk>
2026-09-23 13:21:37 +0100

    chang has been made
    Signed-off-by: thomas gasseling <thomas.gasseling@autotrader.co.uk>

--- session-1/src/main/java/HelloWorld.java
+++ session-1/src/main/java/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     static void main() {
         System.out.println("Hello SD13");
         System.out.println("Hello world");
+        System.out.println("thing 1");
     }
 }
