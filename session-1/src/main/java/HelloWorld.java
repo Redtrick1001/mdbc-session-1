653715b564d36bd5f909d72690a5562108767095
Redtrick <redtrick1001@gmail.com>
2026-09-23 16:30:12 +0100

    Completed task on maps and set
    Signed-off-by: thomas gasseling <thomas.gasseling@autotrader.co.uk>

--- session-1/src/main/java/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-    static void main() {
-        System.out.println("Hello SD13");
-        System.out.println("Hello world");
-        System.out.println("thing 1");
-    }
-}
