dab22178194d6765069176e17682a73965b3a0fd
Redtrick <redtrick1001@gmail.com>
2026-09-24 09:29:52 +0100

    fix formating
    Signed-off-by: thomas gasseling <thomas.gasseling@autotrader.co.uk>

--- session-1/src/main/java/com/mdigital/MultiDimensionalArrayTask.java
+++ session-1/src/main/java/com/mdigital/MultiDimensionalArrayTask.java
@@ -19,7 +19,7 @@ public class MultiDimensionalArrayTask {
         int[][] numbers = {
                 {1, 2},
                 {3, 4},
-                {5,6}
+                {5, 6}
         };
 
         for (int[] num: numbers) {
